f2b922b9822393899f23a731b1ee6bb508fc817f
Nikita Mospan <mospan.nn@phystech.edu>
2017-01-27 22:23:29 +0300

    Add Unit Tests for GameField#computeNextLifeState method

---
+++ src/test/java/mospan/lifegame/GameFieldTest.java
@@ -0,0 +1,72 @@
+package mospan.lifegame;
+
+import org.junit.*;
+
+public class GameFieldTest {
+    private static GameField gameField;
+    private static final int FIELD_WIDTH = 10;
+    private static final int FIELD_HEIGHT = 10;
+
+    private void cleanupGameField () {
+        for (int colIndex = 0; colIndex < FIELD_WIDTH; colIndex++) {
+            for (int rowIndex = 0; rowIndex < FIELD_HEIGHT; rowIndex++) {
+                gameField.getCells().get(colIndex).get(rowIndex).set(LifeState.DEAD);
+            }
+
+        }
+    }
+
+    @BeforeClass
+    public static void beforeClass() {
+        gameField = new GameField(FIELD_WIDTH, FIELD_HEIGHT);
+    }
+
+    @Before
+    public void before() {
+        cleanupGameField();
+    }
+
+    @After
+    public void after() {
+        cleanupGameField();
+    }
+
+    @Test
+    //  1 - means cell is alive
+    //  0 - means cell is dead
+    //  010         000
+    //  010   -->   111
+    //  010         000
+    public void computeNextLifeStateSimpleOscillator() {
+        //init
+        gameField.getCells().get(1).get(0).set(LifeState.ALIVE);
+        gameField.getCells().get(1).get(1).set(LifeState.ALIVE);
+        gameField.getCells().get(1).get(2).set(LifeState.ALIVE);
+
+        //verify
+        Assert.assertEquals(gameField.computeNextLifeState(1, 0), LifeState.DEAD);
+        Assert.assertEquals(gameField.computeNextLifeState(1, 1), LifeState.ALIVE);
+        Assert.assertEquals(gameField.computeNextLifeState(1, 2), LifeState.DEAD);
+
+        Assert.assertEquals(gameField.computeNextLifeState(0, 1), LifeState.ALIVE);
+        Assert.assertEquals(gameField.computeNextLifeState(2, 1), LifeState.ALIVE);
+    }
+
+    @Test
+    //  11   -->   11
+    //  11   -->   11
+    public void computeNextLifeStateSquare() {
+        //init
+        gameField.getCells().get(0).get(0).set(LifeState.ALIVE);
+        gameField.getCells().get(0).get(1).set(LifeState.ALIVE);
+        gameField.getCells().get(1).get(0).set(LifeState.ALIVE);
+        gameField.getCells().get(1).get(1).set(LifeState.ALIVE);
+
+        //verify
+        Assert.assertEquals(gameField.computeNextLifeState(0, 0), LifeState.ALIVE);
+        Assert.assertEquals(gameField.computeNextLifeState(0, 1), LifeState.ALIVE);
+        Assert.assertEquals(gameField.computeNextLifeState(1, 0), LifeState.ALIVE);
+        Assert.assertEquals(gameField.computeNextLifeState(1, 1), LifeState.ALIVE);
+
+    }
+}
